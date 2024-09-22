75c19d7b0407c10fca42002d6e20ad5051b891db
Yamil Andres Honaine Borquez <YA1000>
2024-09-22 16:40:41 -0300

    Despliege

--- main.py
+++ main.py
@@ -28,5 +28,6 @@ def read_root():
     return HTMLResponse('<h2> Hola Mundo! </h2>')
 
 if __name__=='__main__':
+    import uvicorn
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
